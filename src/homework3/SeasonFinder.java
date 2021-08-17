a0e38f9e63b68b7def284761ed9d1e288983eea4
dimatkachenko <demitry.tkachenko@gmail.com>
2021-08-17 14:37:54 +0300

    change if else to switch

--- src/homework3/SeasonFinder.java
+++ src/homework3/SeasonFinder.java
@@ -3,7 +3,6 @@ package homework3;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
 
 public class SeasonFinder {
 
@@ -11,7 +10,8 @@ public class SeasonFinder {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter month number to know what season is it: ");
-        int month = input.nextInt();
+        String month = input.nextLine();
+
 
         Map<Integer, String> monthes = new HashMap();
         monthes.put(1, "January");
@@ -27,33 +27,45 @@ public class SeasonFinder {
         monthes.put(11, "November");
         monthes.put(12, "December");
 
-        if (month == 1) {
-            System.out.println(monthes.get(1) + " it`s Winter time!");
-        } else if (month == 2) {
-            System.out.println("Winter time. Brrr, it`s rather cold at " + monthes.get(2));
-        } else if (month == 3) {
-            System.out.println("Finally Spring " + monthes.get(3));
-        } else if (month == 4) {
-            System.out.println("Spring time! Prepare to 1st " + monthes.get(4) + " fools day");
-        } else if (month == 5) {
-            System.out.println("Spring is almost ended, time to prepare for summer. " + monthes.get(5) + " is now");
-        } else if (month == 6) {
-            System.out.println(monthes.get(6) + " is the first month of Summer");
-        } else if (month == 7) {
-            System.out.println(monthes.get(7) + " is 2nd month of Summer. Time to take a vacation");
-        } else if (month == 8) {
-            System.out.println(monthes.get(8) + " is the last month of Summer, don`t waste time!");
-        } else if (month == 9) {
-            System.out.println(monthes.get(9) + " is the first month of Fall");
-        } else if (month == 10) {
-            System.out.println(monthes.get(10) + " is Fall mid season");
-        } else if (month == 11) {
-            System.out.println(monthes.get(11) + " winter is coming... Fall almost ended");
-        } else if (month == 12) {
-            System.out.println("Winter time! Keep calm and prepare to xMass at " + monthes.get(12));
-        }
-        else {
-            System.out.println("There are only 12 month ia a year");
+        switch (month) {
+            case "1":
+                System.out.println(monthes.get(1) + " it`s Winter time!");
+                break;
+            case "2":
+                System.out.println("Winter time. Brrr, it`s rather cold at " + monthes.get(2));
+                break;
+            case "3":
+                System.out.println("Finally Spring " + monthes.get(3));
+                break;
+            case "4":
+                System.out.println("Spring time! Prepare to 1st " + monthes.get(4) + " fools day");
+                break;
+            case "5":
+                System.out.println("Spring is almost ended, time to prepare for summer. " + monthes.get(5) + " is now");
+                break;
+            case "6":
+                System.out.println(monthes.get(6) + " is the first month of Summer");
+                break;
+            case "7":
+                System.out.println(monthes.get(7) + " is 2nd month of Summer. Time to take a vacation");
+                break;
+            case "8":
+                System.out.println(monthes.get(8) + " is the last month of Summer, don`t waste time!");
+                break;
+            case "9":
+                System.out.println(monthes.get(9) + " is the first month of Fall");
+                break;
+            case "10":
+                System.out.println(monthes.get(10) + " is Fall mid season");
+                break;
+            case "11":
+                System.out.println(monthes.get(11) + " winter is coming... Fall almost ended");
+                break;
+            case "12":
+                System.out.println("Winter time! Keep calm and prepare to xMass at " + monthes.get(12));
+                break;
+            default:
+                System.out.println("Sorry, there are only 12 month ia a year");
         }
     }
 }
