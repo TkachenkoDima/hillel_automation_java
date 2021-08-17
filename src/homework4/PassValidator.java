fbaa458cc73e8cbea53daf157f967b3eaa4a0f9e
dimatkachenko <demitry.tkachenko@gmail.com>
2021-08-17 18:48:37 +0300

    text changes

--- src/homework4/PassValidator.java
+++ src/homework4/PassValidator.java
@@ -8,18 +8,18 @@ public class PassValidator {
         Scanner input = new Scanner(System.in);
 
         for (int counter = 0; counter < 3; counter++) {
-            System.out.println("Knock knock, NEO!");
+            System.out.println("Knock knock, NEO! Enter Pass:");
 
             String correctPass = "kek";
             String inputPass = input.nextLine();
 
             if ((counter == 2)) {
-                System.out.println("Sorry, Mr.Andersen, you are busted");
+                System.out.println("Sorry, Mr.Andersen, you are busted.");
                 return;
             } else if (!inputPass.equals(correctPass)) {
                 System.out.println("Wrong pass, try again");
             } else {
-                System.out.println("Correct! Follow the white rabbit");
+                System.out.println("Correct! Follow the white rabbit.");
                 break;
             }
         }
