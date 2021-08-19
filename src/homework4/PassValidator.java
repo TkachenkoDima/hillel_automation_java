0017b2111da34a40e6531d7ce6172ac0fb1468a5
dimatkachenko <demitry.tkachenko@gmail.com>
2021-08-19 11:23:29 +0300

    refactoring input pass tries

--- src/homework4/PassValidator.java
+++ src/homework4/PassValidator.java
@@ -6,14 +6,14 @@ public class PassValidator {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Knock knock, NEO! Enter Pass:");
 
-        for (int counter = 0; counter < 3; counter++) {
-            System.out.println("Knock knock, NEO! Enter Pass:");
+        for (int counter = 1; true; counter++) {
 
             String correctPass = "kek";
             String inputPass = input.nextLine();
 
-            if ((counter == 2)) {
+            if ((counter == 3) && !inputPass.equals(correctPass)) {
                 System.out.println("Sorry, Mr.Andersen, you are busted.");
                 return;
             } else if (!inputPass.equals(correctPass)) {
