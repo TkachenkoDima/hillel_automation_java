7c121073e590d4909ff863590ca6130fecb935ff
dimatkachenko <demitry.tkachenko@gmail.com>
2021-09-22 12:01:14 +0300

    HW8 init

---
+++ src/homework8/Ghost.java
@@ -0,0 +1,18 @@
+package homework8;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Ghost {
+
+    public Ghost() {
+    }
+
+    public String getColor() {
+        String[] arr = {"White", "Green", "Blue", "Red", "Pink"};
+        int randIdx = ThreadLocalRandom.current().nextInt(arr.length);
+        return arr[randIdx];
+    }
+}
+
+
+
---
+++ src/homework8/MainClass.java
@@ -0,0 +1,12 @@
+package homework8;
+
+public class MainClass extends Ghost{
+
+    public static void main(String[] args) {
+        Ghost ghost = new Ghost();
+        System.out.println("Ghost color is: " + ghost.getColor());
+    }
+}
+
+
+
